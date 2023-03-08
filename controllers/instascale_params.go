748258ee66f729d450fb4ed8ece472813832f5ae
ted chang <htchang@us.ibm.com>
2023-03-07 16:38:58 -0800

    Update instaScale crd and controller
    Signed-off-by: ted chang <htchang@us.ibm.com>

--- controllers/instascale_params.go
+++ controllers/instascale_params.go
@@ -3,14 +3,16 @@ package controllers
 import (
 	mf "github.com/manifestival/manifestival"
 	instascalev1alpha1 "github.com/project-codeflare/codeflare-operator/api/v1alpha1"
+	v1 "k8s.io/api/core/v1"
 )
 
 type InstaScaleParams struct {
-	Name               string
-	Namespace          string
-	Owner              mf.Owner
-	EnableMonitoring   bool
-	MaxScaleoutAllowed int
+	Name                string
+	Namespace           string
+	Owner               mf.Owner
+	EnableMonitoring    bool
+	MaxScaleoutAllowed  int
+	ControllerResources v1.ResourceRequirements
 }
 
 func (p *InstaScaleParams) ExtractParams(instascale *instascalev1alpha1.InstaScale) error {
@@ -19,6 +21,6 @@ func (p *InstaScaleParams) ExtractParams(instascale *instascalev1alpha1.InstaSca
 	p.Owner = instascale
 	p.EnableMonitoring = instascale.Spec.EnableMonitoring
 	p.MaxScaleoutAllowed = instascale.Spec.MaxScaleoutAllowed
-
+	p.ControllerResources = instascale.Spec.ControllerResources
 	return nil
 }
